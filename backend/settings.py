183c41058025e52d5276e6edb27a15ab5d7c62c2
unknown <fasnaramzu2003@gmail.com>
2026-05-04 20:25:19 +0530

    fix BASE_DIR syntax

--- backend/settings.py
+++ backend/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 from pathlib import Path
 
-BASE_DIR =
-Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
